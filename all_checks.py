8763a1397799ed1cd2dcfbda97497ab34221a04c
Marcin Zielinski <czolgista83@gmail.com>
2020-02-25 16:23:14 +0000

    Iterate over a list of checks and messages to avoid code duplication

--- all_checks.py
+++ all_checks.py
@@ -28,9 +28,12 @@ def main():
         (check_reboot, 'Pending Reboot'),
         (check_root_full, 'Root partition full')
     ]
+    everything ok = True
     for check, msg in checks:
         if check():
             print(msg)
+            everything_ok = False
+        if not everything_ok:
             sys.exit(1)
     print('Everything ok.')
     sys.exit(0)
